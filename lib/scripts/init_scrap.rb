b6d3279225654842253910467b21aec7569ab0c4
Hugo Petament <h.petament@icloud.com>
2023-07-29 18:35:34 +0200

    fix init_scrap speed. added 0.15s sleep to avoid to reach api limit) , set sleep to 0.5s with Parallels

--- lib/scripts/init_scrap.rb
+++ lib/scripts/init_scrap.rb
@@ -8,8 +8,8 @@ def feed_first_time(gems)
   counter_changelog = 0
   counter_documentation = 0
   counter_nil = 0
-  gems.each do |gem_name, gem_info|
-    sleep(0.15)
+  Parallel.each(gems, in_threads: 4) do |gem_name, gem_info|
+    sleep(0.5)
     counter_gems += 1
     name = gem_name
     puts name
