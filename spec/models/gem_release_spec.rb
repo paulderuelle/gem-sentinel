a35a001256a7d7d332eccdb2bf072914523592dd
Audrey Pachol <audrey.pachol@gmail.com>
2023-07-29 08:25:03 +0200

    Create Rspec for project model update
    Signed-off-by: Audrey Pachol <audrey.pachol@gmail.com>

--- spec/models/gem_release_spec.rb
+++ spec/models/gem_release_spec.rb
@@ -1,13 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe GemRelease, type: :model do
-  describe 'validations' do
-    it { should validate_presence_of(:version) }
-    it { should validate_uniqueness_of(:version).scoped_to(:master_gem_id) }
-  end
-
-  describe 'associations' do
-    it { should belong_to(:master_gem) }
-    it { should have_many(:project_gems) }
-  end
 end
