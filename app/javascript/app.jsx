31a150418faaecbeb14be22a4c3f1c00b4ed184e
paulderuelle <p.deruelle.mail@gmail.com>
2023-07-29 11:20:14 +0200

    basic behavior of react containers

--- app/javascript/app.jsx
+++ app/javascript/app.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Containers from './components/containers.jsx';
 
-// Render un component
 const productContainer = document.getElementById('main-container');
 const productRoot = createRoot(productContainer);
 
