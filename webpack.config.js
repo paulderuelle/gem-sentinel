719c557d96c0d18d00bc457ef389a296222c4f38
Hugo Petament <h.petament@icloud.com>
2023-07-30 12:22:24 +0200

    useless commments removed

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  mode: "development", // Définissez le mode sur "development" pour le développement
+  mode: "development",
   devtool: "source-map",
   entry: {
     application: "./app/javascript/application.js"
