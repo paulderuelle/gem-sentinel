cb2aa99955ebb70142f152329a9add748ef70b82
Hugo Petament <h.petament@icloud.com>
2023-07-30 13:36:16 +0200

    set method show in project_gems to get the displayed data in 'changelog_page

--- app/controllers/api/v1/gem_releases_controller.rb
+++ app/controllers/api/v1/gem_releases_controller.rb
@@ -1,6 +1,3 @@
 class Api::V1::GemReleasesController < ApplicationController
-  def show
-    gem_release = GemRelease.find(params[:id])
-    render json: gem_release
-  end
+  
 end
--- app/controllers/api/v1/project_gems_controller.rb
+++ app/controllers/api/v1/project_gems_controller.rb
@@ -14,11 +14,29 @@ class Api::V1::ProjectGemsController < ApplicationController
     render json: gem_data
   end
 
+  def show
+    project_gem = ProjectGem.find(params[:id])
+
+    data_to_display_in_project_gem_show = {
+      name: project_gem.master_gem.name,
+      reference_version: get_master_gem_gem_release(project_gem).version,
+      current_version: project_gem.gem_release.version,
+      changelog_page_url: get_master_gem_gem_release(project_gem).changelog_page_url,
+      documentation_page_url: get_master_gem_gem_release(project_gem).documentation_page_url
+    }
+
+    render json: data_to_display_in_project_gem_show
+  end
+
   private
 
   def get_gem_status(project_gem)
-    master_gem_version = project_gem.master_gem.gem_releases.order(created_at: :desc).first.version
+    master_gem_version = get_master_gem_gem_release(project_gem).version
     project_gem_version = project_gem.gem_release.version
     master_gem_version == project_gem_version ? 'Up to date' : 'Updatable'
+  end
+
+  def get_master_gem_gem_release(project_gem)
+    project_gem.master_gem.gem_releases.order(created_at: :desc).first
   end
 end
